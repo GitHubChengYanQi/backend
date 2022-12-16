deb40a4e2f25a8d2c6ad3b594da5502b7f3626fb
zhongbao <suzhongbao@mokahr.com>
2022-12-16 16:19:43 +0800

    ✨ 联合用药列表联调

---
+++ src/views/mall/combination/components/diseaseSelect.vue
@@ -0,0 +1,37 @@
+<template>
+  <a-cascader
+    :field-names="{ label: 'name', value: 'id', children: 'children' }"
+    v-model="value"
+    @change="onChange"
+    :options="dataList"
+    :placeholder="placeholder" />
+</template>
+<script>
+import { treeList } from '@/api/mall'
+export default {
+  props: ['placeholder', 'value'],
+  data () {
+    return {
+      dataList: []
+    }
+  },
+  mounted () {
+    this.getDiseaseList()
+  },
+  methods: {
+    onChange (value) {
+      this.$emit('change', value)
+    },
+    getDiseaseList () {
+      // 过滤下children为空的字段
+      treeList().then((res) => {
+        this.dataList = res.data.children
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
