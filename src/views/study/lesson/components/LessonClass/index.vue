b719f5e9cd44ead4d2e498f16697c55b2df364b9
cheng <2683941980@qq.com>
2023-01-04 17:04:02 +0800

    去掉element ui组件

--- src/views/study/lesson/components/LessonClass/index.vue
+++ src/views/study/lesson/components/LessonClass/index.vue
@@ -1,76 +1,82 @@
 <template>
   <div class="lessonClass">
     <a-spin :spinning="tableLoading">
-      <el-tree
-        class="tree"
-        :data="data"
-        icon-class="icon"
-        node-key="key"
-        @node-drag-start="handleDragStart"
-        @node-drag-enter="handleDragEnter"
-        @node-drag-leave="handleDragLeave"
-        @node-drag-over="handleDragOver"
-        @node-drag-end="handleDragEnd"
-        @node-drop="handleDrop"
-        draggable
-        :allow-drop="allowDrop"
-      >
-        <div
-          class="custom-tree-node"
-          slot-scope="{ node, data }"
-          @mouseenter="mouseenter(node)"
-          @mouseleave="mouseleave(node)"
-
-        >
-          <div class="classRow">
-            <DragIcon></DragIcon>
-            <span class="className">{{ data.title }}</span>
-          </div>
-          <div class="my-space">
-            <div v-if="visible === node.key" style="padding-right: 8px">
-              <el-button
-                v-if="data.level === 1"
-                type="text"
-                size="mini"
-                @click="() => {
-                  className = ''
-                  parentClassId = data.key
-                  openAddChildrenClass = true
-                }">
-                <a-icon type="plus-circle" />
-              </el-button>
-              <el-button
-                type="text"
-                size="mini"
-                @click="() => update(node, data)">
-                <a-icon type="edit" />
-              </el-button>
-              <el-button
-                type="text"
-                size="mini"
-                @click="() => remove(node, data)">
-                <a-icon type="delete" />
-              </el-button>
+      <div class="lessonClass1">
+        <div v-for="data in treeData" :key="data.key">
+          <div
+            class="custom-tree-node"
+            :style="{backgroundColor:visible === data.key ? '#eee' : '#fff'}"
+            @mouseenter="mouseenter(data)"
+            @mouseleave="mouseleave(data)"
+          >
+            <div class="classRow">
+              <DragIcon class="my-handle"></DragIcon>
+              <span class="className">{{ data?.title }}</span>
             </div>
-            <div v-if="Array.isArray(data.children) && data.children.length > 0">
-              <el-button
-                v-if="!node.expanded"
-                type="text"
-                size="mini"
+            <div class="my-space">
+              <div v-if="visible === data.key">
+                <a-button
+                  type="link"
+                  @click="add(data)">
+                  <a-icon type="plus-circle" />
+                </a-button>
+                <a-button
+                  type="link"
+                  @click="() => update(data)">
+                  <a-icon type="edit" />
+                </a-button>
+                <a-button
+                  type="link"
+                  @click="() => remove(data)">
+                  <a-icon type="delete" />
+                </a-button>
+              </div>
+              <div v-if="Array.isArray(data.children) && data.children.length > 0">
+                <a-button
+                  type="link"
+                  @click="expanded(data,data.key === expandedKey)"
+                >
+                  <a-icon v-if="data.key !== expandedKey" type="left" />
+                  <a-icon v-else type="down" />
+                </a-button>
+              </div>
+            </div>
+          </div>
+
+          <div class="lessonClass2" v-if="data.key === expandedKey">
+            <div
+              v-for="childrenItem in data.children"
+              :key="childrenItem.key"
+              :style="{backgroundColor:visible === childrenItem.key ? '#eee' : '#fff',paddingLeft: '18px'}"
+            >
+              <div
+                class="custom-tree-node"
+                @mouseenter="mouseenter(childrenItem)"
+                @mouseleave="mouseleave(childrenItem)"
               >
-                <a-icon type="left" />
-              </el-button>
-              <el-button
-                v-else
-                type="text"
-                size="mini"
-              >
-                <a-icon type="down" />
-              </el-button>
+                <div class="classRow">
+                  <DragIcon class="my-handle"></DragIcon>
+                  <span class="className">{{ childrenItem?.title }}</span>
+                </div>
+                <div class="my-space">
+                  <div v-if="visible === childrenItem.key" style="padding-right: 8px">
+                    <a-button
+                      type="link"
+                      @click="() => update(childrenItem)">
+                      <a-icon type="edit" />
+                    </a-button>
+                    <a-button
+                      type="link"
+                      @click="() => remove(childrenItem)">
+                      <a-icon type="delete" />
+                    </a-button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </el-tree>
+      </div>
     </a-spin>
 
     <a-button
@@ -155,6 +161,7 @@ import {
   courseClassUpdate
 } from '@/api/study/lessonClass'
 import { message } from 'ant-design-vue'
+import Sortable from 'sortablejs'
 
 export default {
   data () {
@@ -171,7 +178,8 @@ export default {
       openDelClass: false,
       openAddChildrenClass: false,
       visible: -1,
-      data: [],
+      expandedKey: 0,
+      treeData: [],
       defaultProps: {
         children: 'children',
         label: 'label'
@@ -191,9 +199,21 @@ export default {
           }
           return data.map(item => ({ ...item, level, children: formatTree(item.children, level + 1) }))
         }
-        this.data = formatTree(res.data, 1)
+        this.treeData = formatTree(res.data, 1)
       }).finally(() => {
         this.tableLoading = false
+        // 获取节点
+        const node = document.querySelector('.lessonClass1')
+        // 拖拽排序
+        Sortable.create(node, {
+          delay: 0,
+          handle: '.my-handle',
+          animation: 300, // 拖拽延时，效果更好看
+          onEnd: async (evt) => {
+            this.treeData.splice(evt.newIndex, 0, this.treeData.splice(evt.oldIndex, 1)[0])
+            this.handleDragEnd()
+          }
+        })
       })
     },
     addClass (pid) {
@@ -233,31 +253,51 @@ export default {
         this.loading = false
       })
     },
-    mouseenter (node) {
-      this.visible = node.key
+    mouseenter (data) {
+      this.visible = data.key
     },
-    mouseleave (node) {
+    mouseleave () {
       this.visible = -1
     },
-    update (node) {
-      this.updateClass = { ...node.data }
+    add (data) {
+      this.className = ''
+      this.parentClassId = data.key
+      this.openAddChildrenClass = true
+    },
+    update (data) {
+      this.updateClass = { ...data }
       this.openUpdateClass = true
     },
-    remove (node) {
-      this.delClass = node.data
+    remove (data) {
+      this.delClass = data
       this.openDelClass = true
     },
-    handleDragStart (node, ev) {
-
-    },
-    handleDragEnter (draggingNode, dropNode, ev) {
-
-    },
-    handleDragLeave (draggingNode, dropNode, ev) {
-
-    },
-    handleDragOver (draggingNode, dropNode, ev) {
-
+    expanded (data, expanded) {
+      this.expandedKey = expanded ? 0 : data.key
+      if (!expanded) {
+        setTimeout(() => {
+          // 获取节点
+          const node = document.querySelector('.lessonClass2')
+          // 拖拽排序
+          Sortable.create(node, {
+            delay: 0,
+            handle: '.my-handle',
+            animation: 300, // 拖拽延时，效果更好看
+            onEnd: async (evt) => {
+              const newTreeData = this.treeData.map((item) => {
+                if (item.key === data.key) {
+                  const children = item.children || []
+                  children.splice(evt.newIndex, 0, children.splice(evt.oldIndex, 1)[0])
+                  return { ...item, children }
+                }
+                return item
+              })
+              this.treeData = newTreeData
+              this.handleDragEnd()
+            }
+          })
+        }, 0)
+      }
     },
     handleDragEnd () {
       const newData = []
@@ -274,7 +314,7 @@ export default {
           formatTree(item.children)
         })
       }
-      formatTree(this.data)
+      formatTree(this.treeData)
       this.tableLoading = true
       courseClassSort({ params: newData }).then(() => {
         message.success('排序成功！')
@@ -283,16 +323,6 @@ export default {
       }).finally(() => {
         this.tableLoading = false
       })
-    },
-    handleDrop (draggingNode, dropNode, dropType, ev) {
-
-    },
-    allowDrop (draggingNode, dropNode, type) {
-      if (draggingNode.level === dropNode.level) {
-        return type !== 'inner'
-      } else {
-        return false
-      }
     }
   },
   components: { DragIcon }
@@ -432,30 +462,13 @@ export default {
     }
   }
 }
+
 .lessonClass {
   max-height: 400px;
   overflow: auto;
-}
 
-.tree {
-  /deep/ .el-tree-node__content {
-    height: auto;
-    width: 100%;
-    align-items: normal;
-
-    .custom-tree-node {
-      width: 100%;
-      padding: 0 12px;
-    }
-
-    .el-tree-node__expand-icon {
-      padding: 0;
-    }
-
-    .el-tree-node__expand-icon.is-leaf {
-      min-height: 100%;
-      padding: 0;
-    }
+  .ant-btn {
+    padding: 0 4px;
   }
 }
 
@@ -465,8 +478,8 @@ export default {
   align-items: center;
   justify-content: space-between;
   font-size: 14px;
-  padding: 8px 12px 8px 0;
-  height: 40px
+  padding: 8px 12px;
+  height: 40px;
 }
 
 .classRow {
