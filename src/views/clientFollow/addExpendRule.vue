6aed2d2cf2372caa698a874c85f91bf0826e219c
ligk <2662175556@qq.com>
2023-01-06 15:23:05 +0800

    消费属性标签问题修复

--- src/views/clientFollow/addExpendRule.vue
+++ src/views/clientFollow/addExpendRule.vue
@@ -17,7 +17,7 @@
               <span>*</span> 规则名称：
             </div>
             <a-input
-              style="width: 400px; height: 36px"
+              style="width: 500px; height: 36px"
               placeholder="请填写规则名称，仅内部可见"
               v-model="form.name"
               :maxLength="30"
@@ -31,7 +31,7 @@
             <div class="label">
               <span>*</span> 选择标签：
             </div>
-            <LabelSelect v-model="form.labelIdGroup" :addState="true" style="width:400px;" />
+            <LabelSelect v-model="form.labelIdGroup" :addState="true" style="width:500px;" />
           </div>
           <!-- end 选择标签 -->
 
@@ -97,6 +97,7 @@
           type="primary"
           :loading="loading"
           @click="addRule('task')"
+          v-if="btnShow"
         >保存并创建跟客任务</a-button>
       </div>
     </a-card>
@@ -157,12 +158,19 @@ import {
   consumeAutoLabelUpdate,
   consumeAutoLabelValidRule
 } from '@/api/clientFollow.js'
+import { mapGetters } from 'vuex'
 
 export default {
   components: { LabelSelect, clientAttr, GoodsSelect, goodsAttr },
+  computed: {
+    ...mapGetters([
+      'permissionList'
+    ])
+  },
   data () {
     return {
       loading: false,
+      btnShow: false,
       id: '', // 类别
       label: '', // id
       labelCol: { span: 4 },
@@ -199,6 +207,14 @@ export default {
     this.label = this.$route.query.label
     this.getDict('good_bounds')
     this.getDetail()
+    // 根据sop权限判断是否显示按钮“保存并创建跟客任务”
+    const permissinon = this.permissionList[0].routes
+    if (permissinon.indexOf('/sales/sop') > -1) {
+      this.btnShow = true
+    } else {
+      this.btnShow = false
+    }
+
   },
   methods: {
     deepClonev2,
