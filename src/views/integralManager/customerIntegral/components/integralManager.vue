bc5d7af709992b3526c7b5fd09a41748103b0e69
“jingxin” <jingxin@yifeijiankang.com>
2023-01-07 15:49:54 +0800

    增加积分管理和积分统计页面

--- src/views/integralManager/customerIntegral/components/integralManager.vue
+++ src/views/integralManager/customerIntegral/components/integralManager.vue
@@ -22,6 +22,9 @@
         style="margin-right: 10px;"
       >导出</a-button>
     </div>
+    <div class="searchLine">
+      <a-button class="batchButtonDiv" @click="goBatchChangeIntegral">批量调整积分</a-button>
+    </div>
     <a-table
       :loading="tableLoading"
       :row-key="record => record.id"
@@ -39,16 +42,94 @@
         </template>
       </div>
     </a-table>
+    <a-modal
+      title="批量调整积分"
+      :maskCloseable="false"
+      :width="1000"
+      :visible="integralBatchShowStatus"
+      class="batchModalClass"
+      @cancel="closeIntegralBatchModal()"
+      :getContainer="() => $refs['integral_manager_container']"
+    >
+      <div class="formDivContent">
+        <div class="formDivText">将给 1 位员工调整积分，员工可在积分统计查看相应调整记录</div>
+        <div class="singleFormRulesDiv">
+          <div class="singleFormTitle">调整积分</div>
+          <div class="singleFormContent">
+            <div class="singleRulesContent">
+              <a-select class="singleInputClass" placeholder="请选择"></a-select>
+              <a-input addon-after="积分" default-value="10" class="singleInputClass"></a-input>
+              <!-- <div class="singleFormText">好友查看了</div> -->
+              <!-- <a-input placeholder="请选择互动雷达"></a-input> -->
+            </div>
+            <div class="singleRulesText">当扣减的积分大于员工积分余额时，员工积分将会直接调整为0</div>
+          </div>
+        </div>
+        <div class="singleFormRulesDiv">
+          <div class="singleFormTitle">调整原因</div>
+          <div class="singleFormContent">
+            <div class="singleRulesContent">
+              <a-textarea placeholder="请输入原因" class="rasonTextareaDiv"></a-textarea>
+              <div class="len">0/18</div>
+            </div>
+            <div class="singleReasonContent">
+              <div class="reasonTopDiv">
+                <div class="reasonTitle">历史调整原因</div>
+                <div class="reasonEditButton">编辑</div>
+              </div>
+              <div class="reasonTagDiv">
+                <div class="singleReasonTag">显示历史调整原因</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <template slot="footer">
+        <a-button
+          @click="closeIntegralBatchModal"
+        >取消</a-button>
+        <a-button type="primary" @click="confirmIntegralBatch">确定</a-button>
+      </template>
+    </a-modal>
+    <!-- <SelectModal
+      @output="getKeys"
+      :selectedKeys="keys"
+      :selectedRows="rows"
+      :placeholder="searchPlaceholder"
+      :list="treeData"
+      :fieldNames="fieldNames"
+      :transferTip="transferTip"
+      ref="SelectPersonnel" /> -->
+      <SelectModal
+      :selectedKeys="keys"
+      :selectedRows="rows"
+      @output="getKeys"
+      :list="treeData"
+      ref="SelectPersonnel" />
   </div>
 </template>
 
 <script>
+import { departmentEmp } from '@/api/common.js'
+import SelectModal from '@/components/SelectPersonnel/components/modal'
 import { getTempIntegralManagerData } from '@/api/integralManager'
 export default {
   name: 'BackendIntegralManager',
-
+  components: {
+    SelectModal
+  },
+  props: {
+    multiple: {
+      type: Boolean,
+      default: true
+    }
+  },
   data () {
     return {
+      keys: [],
+      rows: [],
+      treeData: [],
+      integralBatchShowStatus: false,
       searchInfo: {},
       employeeIds: [],
       // 表格加载效果
@@ -128,6 +209,54 @@ export default {
   },
 
   methods: {
+    goBatchChangeIntegral () {
+      this.$refs.SelectPersonnel.modalShow = true
+      departmentEmp().then(res => {
+        // this.treeData = res.data
+        // this.loading = false
+        this.treeData = this.formatTree(res.data)
+        // this.rows = this.getNodes(this.treeData, this.value)[0]
+        // sessionStorage.setItem(this.curId, JSON.stringify(this.keys))
+        // this.$refs.SelectPersonnel.modalShow = !state
+      })
+      // this.$refs.selectEmployee.$show([])
+    },
+    formatTree (data) {
+      const arr = []
+      for (let i = 0; i < data.length; i++) {
+        data[i].leaf = data[i].isLeaf
+        delete data[i].isLeaf
+        data[i].name = data[i].title
+        data[i].label = data[i].title
+        data[i].scopedSlots = { title: 'title' }
+        arr.push(data[i])
+        if (data[i].children.length === 0 && data[i].leaf === '0') {
+          data[i].disabled = true
+        }
+        if (data[i].children && data[i].children.length > 0) {
+          this.formatTree(data[i].children)
+        }
+      }
+      return arr
+    },
+    getKeys (e, type) {
+      console.log(e, type, '选中回调')
+    },
+    selectEmployeeChange (e) {
+      console.log(e, '批量处理选择')
+    },
+    // 调整积分弹框点击取消
+    closeIntegralBatchModal () {
+      this.integralBatchShowStatus = false
+    },
+    // 调整积分弹框点击确定
+    confirmIntegralBatch () {
+      this.integralBatchShowStatus = false
+    },
+    // 点击调整积分
+    setIntegral () {
+      this.integralBatchShowStatus = true
+    },
     // // 获取数据
     // async getTableData () {
     //   this.tableLoading = true
@@ -161,22 +290,22 @@ export default {
     //   // 临时接收假数据
     //   // this.tableData = getTempSopList()
     // },
-    // // 群SOP模板切换页码
-    // handleTableChange ({ current, pageSize }, filters, sorter) {
-    //   this.pagination.current = current
-    //   this.pagination.pageSize = pageSize
-    //   console.log(sorter, 'sorter')
-    //   if (sorter.order) {
-    //     if (sorter.order === 'ascend') {
-    //       this.sorter = 'asc'
-    //     } else {
-    //       this.sorter = 'desc'
-    //     }
-    //   } else {
-    //     this.sorter = ''
-    //   }
-    //   this.getTableData()
-    // },
+    // 群SOP模板切换页码
+    handleTableChange ({ current, pageSize }, filters, sorter) {
+      this.pagination.current = current
+      this.pagination.pageSize = pageSize
+      console.log(sorter, 'sorter')
+      if (sorter.order) {
+        if (sorter.order === 'ascend') {
+          this.sorter = 'asc'
+        } else {
+          this.sorter = 'desc'
+        }
+      } else {
+        this.sorter = ''
+      }
+      // this.getTableData()
+    },
     // // 搜索
     // goSearch () {
     //   // 重新将页码换成1
